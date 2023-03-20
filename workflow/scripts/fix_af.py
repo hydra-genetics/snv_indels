3f8494fc696c08feac48fab3ef1a3d08b74e95cb
zezzipa <jessika_90@hotmail.com>
2023-03-20 13:54:54 +0100

    Update fix_af.py

--- workflow/scripts/fix_af.py
+++ workflow/scripts/fix_af.py
@@ -28,7 +28,7 @@ def getCaller(path: str):
 
 # modify vcf header if necessary
 def modifyHeader(caller: str, header: pysam.libcbcf.VariantHeader):
-    if caller == "pisces" or caller == "gatk_mutect2":
+    if caller == "pisces" or caller == "gatk_mutect2"  or caller == "pbrun_deepvariant":
         header.info.add("AF", "A", "Float", "DescriptionDescription")
     elif caller == "varscan":
         header.info.add(
@@ -75,12 +75,17 @@ def writeNewVcf(
             row.info["AF"] = row.samples[0].get("VF")
         elif caller == "vardict":
             row.info["AF"] = row.samples[0].get("AF")
+        elif caller == "pbrun_deepvariant":
+            row.info["AF"] = row.samples[0].get("VAF")
+        elif caller == "gatk_select_variants_final":
+            row.info["AF"] = row.samples[0].get("AF")
         elif caller == "varscan":
             row.info["AF"] = row.samples[0].get("AD")/row.samples[0].get("DP")
         else:
             raise ValueError(
-                "{} is not a valid caller for this script. Choose between:"
-                "freebayes, haplotypecaller, gatk_mutect2, pisces, vardict, varscan.".format(caller)
+                "{} is not a valid caller for this script. Choose between: "
+                "freebayes, haplotypecaller, gatk_mutect2, gatk_select_variants_final, "
+                "pbrun_deepvariant, pisces, vardict, varscan.".format(caller)
             )
         new_vcf.write(row)
     return
